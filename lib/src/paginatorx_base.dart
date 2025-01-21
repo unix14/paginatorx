2446fd4369085a45e5288ff0ee2c7b640722441d
WG-EYALYL\EyalY <eyaly@cocacola.co.il>
2025-01-21 15:21:56 +0200

    fixed example code

--- lib/src/paginatorx_base.dart
+++ lib/src/paginatorx_base.dart
@@ -1,5 +1,7 @@
 // TODO: Put public facing types in this file.
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:paginatorx/data/stub_data.dart';
 import 'package:paginatorx/paging/paging_manager.dart';
 import 'package:paginatorx/paging/paging_manager_impl.dart';
@@ -22,9 +24,16 @@ class PaginatorxBase {
     data = pagingManager.loadPrevPage();
 
     // bring data into the widget
-
-
-
+    Container(
+      child: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(data[index].toString()),
+          );
+        },
+      ),
+    );
 
     print('end');
   }
